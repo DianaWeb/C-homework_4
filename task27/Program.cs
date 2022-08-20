e46056623cf76159e3391a80720f526b2cd0445b
«Diana» <diana-mk@ya.ru>
2022-08-20 12:28:02 +0700

    решили домашние задания 4

---
+++ task27/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12 */
+
+int interNumber()
+{
+	System.Console.Write("Введите число: ");
+	int newNumber = Convert.ToInt32(Console.ReadLine());
+	return newNumber;
+}
+
+int sumNumber(int num)
+{
+	num = Math.Abs(num);
+
+	int sum = 0;
+	while (num > 0)
+	{
+		sum = sum + num % 10;
+		num = num / 10;
+	}
+	return sum;
+}
+
+int number = interNumber();
+int sumNum = sumNumber(number);
+System.Console.WriteLine($"Сумма цифр числа: {sumNum}");
+
---
+++ task29/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. */
+int[] array = new int[8];
+for (int i = 0; i < array.Length; i++)
+{
+	System.Console.Write("Введите число 8 раз: ");
+	array[i] = Convert.ToInt32(Console.ReadLine());
+}
+System.Console.WriteLine($"Наш массив [{string.Join(", ", array)}]");
